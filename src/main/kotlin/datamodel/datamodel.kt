c0de461c795f492422814864cce794c6792a8d68
mnzhu16 <638792mz@eur.nl>
2024-11-27 23:35:29 +0100

    create the data model

--- src/main/kotlin/datamodel/datamodel.kt
+++ src/main/kotlin/datamodel/datamodel.kt
@@ -1,4 +1,20 @@
 package com.AH.delivery.datamodel
 
-class datamodel {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+import java.util.UUID
+
+enum class DeliveryStatus{IN_PROGRESS, DELIVERED}
+
+@Entity
+data class Delivery(
+    @Id val id: UUID = UUID.randomUUID(),
+    val vehicleId: String = "null",
+    val startedAt: LocalDateTime = LocalDateTime.now(),
+    var finishedAt: LocalDateTime? = null,
+    @Enumerated(EnumType.STRING)
+    var status: DeliveryStatus = DeliveryStatus.IN_PROGRESS
+)
