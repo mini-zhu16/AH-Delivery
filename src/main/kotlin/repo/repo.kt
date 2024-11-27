011e5a197409aacb91034bb95900ef2eee2abff5
mnzhu16 <638792mz@eur.nl>
2024-11-27 23:44:23 +0100

    create data repository

--- src/main/kotlin/repo/repo.kt
+++ src/main/kotlin/repo/repo.kt
@@ -1,4 +1,10 @@
 package com.AH.delivery.repo
 
-class repo {
-}
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import com.AH.delivery.datamodel.Delivery
+import java.util.*
+
+
+@Repository
+interface DeliveryRepository : JpaRepository<Delivery, UUID>
