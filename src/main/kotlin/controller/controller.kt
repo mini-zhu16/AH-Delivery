5b6824fd8a4b1142eae0b1534224879bab3c253f
mnzhu16 <638792mz@eur.nl>
2024-11-27 23:56:54 +0100

    create the first endpoint

--- src/main/kotlin/controller/controller.kt
+++ src/main/kotlin/controller/controller.kt
@@ -1,4 +1,15 @@
 package com.AH.delivery.controller
 
-class controller {
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import com.AH.delivery.service.DeliveryService
+import com.AH.delivery.datamodel.Delivery
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+
+@RestController
+@RequestMapping("/deliveries")
+class DeliveryController(private val deliveryservice: DeliveryService) {
+    @PostMapping
+    fun createDelivery(@RequestBody delivery: Delivery): Delivery = deliveryservice.save(delivery)
 }
