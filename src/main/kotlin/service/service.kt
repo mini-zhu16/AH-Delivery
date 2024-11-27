e6d694444e3de84650904c984c4a89a3ea9ed919
mnzhu16 <638792mz@eur.nl>
2024-11-27 23:45:13 +0100

    create the first business logic: create delivery instances

--- src/main/kotlin/service/service.kt
+++ src/main/kotlin/service/service.kt
@@ -1,4 +1,17 @@
 package com.AH.delivery.service
 
-class service {
+import com.AH.delivery.datamodel.Delivery
+import com.AH.delivery.datamodel.DeliveryStatus
+import com.AH.delivery.repo.DeliveryRepository
+import org.springframework.stereotype.Service
+
+@Service
+class DeliveryService(private val deliveryrepo: DeliveryRepository) {
+    fun save(delivery: Delivery): Delivery {
+        if (delivery.status != DeliveryStatus.IN_PROGRESS && delivery.status != DeliveryStatus.DELIVERED) {
+            throw IllegalArgumentException("Status must be IN_PROGRESS or DELIVERED")
+        } else {
+            return deliveryrepo.save(delivery)
+        }
+    }
 }
