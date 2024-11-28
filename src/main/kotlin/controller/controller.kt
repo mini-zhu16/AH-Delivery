48de15bf7a4ed8f44ba26985acf3c1ee232b471a
mnzhu16 <638792mz@eur.nl>
2024-11-28 12:43:29 +0100

    add a get mapping in the controller for the last endpoint

--- src/main/kotlin/controller/controller.kt
+++ src/main/kotlin/controller/controller.kt
@@ -3,6 +3,7 @@ package com.AH.delivery.controller
 import com.AH.delivery.service.DeliveryService
 import com.AH.delivery.datamodel.Delivery
 import com.AH.delivery.dto.BulkDeliveryUpdateRequest
+import com.AH.delivery.dto.DeliverySummary
 import com.AH.delivery.dto.DeliveryUpdateRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -26,5 +27,11 @@ class DeliveryController(private val deliveryservice: DeliveryService) {
     fun bulkupdateDelivery(@RequestBody requests: List<BulkDeliveryUpdateRequest>): ResponseEntity<Map<String, List<Delivery>>>  {
         val bulkupdateDelivery = deliveryservice.bulkupdateDelivery(requests)
         return ResponseEntity.ok(mapOf("deliveries" to bulkupdateDelivery))
+    }
+
+    @GetMapping("/business-summary")
+    fun summaryDelivery(): ResponseEntity<DeliverySummary> {
+        val summaryDelivery = deliveryservice.summaryDelivery()
+        return ResponseEntity.ok(summaryDelivery)
     }
 }
