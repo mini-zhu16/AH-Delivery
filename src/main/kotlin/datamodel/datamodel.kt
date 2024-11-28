53e8b7f75914caa2a541f3994c4e2bc64d193ad8
mnzhu16 <638792mz@eur.nl>
2024-11-28 15:28:33 +0100

    change the date type to zoneddatatime to provide more flexibility

--- src/main/kotlin/datamodel/datamodel.kt
+++ src/main/kotlin/datamodel/datamodel.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
 import jakarta.validation.constraints.NotNull
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 import java.util.UUID
 
 enum class DeliveryStatus{IN_PROGRESS, DELIVERED}
@@ -14,8 +14,8 @@ enum class DeliveryStatus{IN_PROGRESS, DELIVERED}
 data class Delivery(
     @Id val id: UUID = UUID.randomUUID(),
     @field: NotNull val vehicleId: String = "",
-    @field: NotNull val startedAt: LocalDateTime = LocalDateTime.now(),
-    var finishedAt: LocalDateTime? = null,
+    @field: NotNull val startedAt: ZonedDateTime = ZonedDateTime.now(),
+    var finishedAt: ZonedDateTime? = null,
     @Enumerated(EnumType.STRING)
     @field:NotNull var status: DeliveryStatus = DeliveryStatus.IN_PROGRESS
 )
--- src/main/kotlin/dto/dto.kt
+++ src/main/kotlin/dto/dto.kt
@@ -1,17 +1,15 @@
 package com.AH.delivery.dto
 
 import com.AH.delivery.datamodel.DeliveryStatus
-import com.fasterxml.jackson.annotation.JsonFormat
 import org.jetbrains.annotations.NotNull
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 import java.util.*
 
 data class DeliveryUpdateRequest(
     @field:NotNull("Status needs to be defined")
     val status: DeliveryStatus,
 
-    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
-    val finishedAt: LocalDateTime?= null
+    val finishedAt: ZonedDateTime?= null
 )
 
 data class BulkDeliveryUpdateRequest(
@@ -21,8 +19,7 @@ data class BulkDeliveryUpdateRequest(
     @field:NotNull("Status needs to be defined")
     val status: DeliveryStatus,
 
-    @field:JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
-    val finishedAt: LocalDateTime?= null
+    val finishedAt: ZonedDateTime?= null
 )
 
 data class DeliverySummary(
