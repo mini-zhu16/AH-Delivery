369f5acc132a0abaa1bfbedd06bd4cd0a7144be6
mnzhu16 <638792mz@eur.nl>
2024-11-28 12:41:41 +0100

    add query to get delivery info from yesterday for the last get endpoint

--- src/main/kotlin/repo/repo.kt
+++ src/main/kotlin/repo/repo.kt
@@ -2,9 +2,23 @@ package com.AH.delivery.repo
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import com.AH.delivery.datamodel.Delivery
+import java.time.LocalDateTime
 import java.util.*
 
 
 @Repository
-interface DeliveryRepository : JpaRepository<Delivery, UUID>
+interface DeliveryRepository : JpaRepository<Delivery, UUID>{
+    @Query ("""SELECT count(d) FROM Delivery d WHERE d.startedAt > :start""")
+    fun countByStartDateAfter(
+        @Param("start") start: LocalDateTime
+    ): Int
+
+    @Query ("""SELECT d FROM Delivery d WHERE d.startedAt > :start""")
+    fun findByStartDateAfter(
+        @Param("start") start: LocalDateTime
+    ): List<Delivery>
+}
+
